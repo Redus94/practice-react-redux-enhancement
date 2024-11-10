152030fdca38603ac0512fca4f1c2767c842886c
Paweł Suder <pawel.suder94@gmail.com>
2024-11-10 10:37:15 +0100

    task 02 done

--- src/hoc/withPopup.js
+++ src/hoc/withPopup.js
@@ -1,8 +1,38 @@
-import React from 'react';
+import React from "react";
 
-const withPopup = () => {
-    return () => <section>withPopup()</section>;
-}
+const withPopup = (styles) => (WrappedComponent) => {
+  return class extends React.Component {
+    state = {
+      open: true,
+    };
 
+    closePopup = () => {
+      this.setState({
+        open: false,
+      });
+    };
+
+    render() {
+      const defaultStyles = {
+        position: "absolute",
+        top: 0,
+        left: 0,
+        right: 0,
+        bottom: 0,
+        background: "rgba(0,0,0,0.8",
+      };
+
+      if (this.state.open === true) {
+        return (
+          <section style={{ ...defaultStyles, ...styles }}>
+            <WrappedComponent />
+            <button onClick={() => this.closePopup()}>X</button>
+          </section>
+        );
+      }
+      return <WrappedComponent />;
+    }
+  };
+};
 
 export default withPopup;
