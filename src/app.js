eac5d5f4207200be14a8abbc89d74f6eff80e748
Paweł Suder <pawel.suder94@gmail.com>
2024-11-10 18:18:22 +0100

    task 03 done

--- src/app.js
+++ src/app.js
@@ -1,8 +1,20 @@
 // ./src/app.js
-import React from 'react';
-import { createRoot } from 'react-dom/client';
+import React from "react";
+import { createRoot } from "react-dom/client";
 
-import App from './components/App';
+import { legacy_createStore as createStore, applyMiddleware } from "redux";
+import thunk from "redux-thunk";
+import { Provider } from "react-redux";
+import { reducer } from "./modules/github";
+import { composeWithDevTools } from "redux-devtools-extension";
 
-const root = createRoot(document.querySelector('#root'));
-root.render(<App />);
+import App from "./components/App";
+
+const store = createStore(reducer, composeWithDevTools(applyMiddleware(thunk)));
+
+const root = createRoot(document.querySelector("#root"));
+root.render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+);
---
+++ src/modules/github/github.js
@@ -0,0 +1,40 @@
+import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { getRepos, setLogin, setPhrase } from "./github.actions";
+
+const GitHub = () => {
+  const { login, phrase, repos } = useSelector((state) => state);
+  const dispatch = useDispatch();
+
+  function handleLoginChange(e) {
+    const login = e.target.value;
+    dispatch(setLogin(login));
+    dispatch(getRepos(login));
+  }
+
+  console.log(repos);
+
+  return (
+    <section>
+      <form>
+        <input value={login} name="login" onChange={handleLoginChange} />
+        <input
+          value={phrase}
+          name="phrase"
+          onChange={(e) => dispatch(setPhrase(e.target.value))}
+        />
+      </form>
+      <ul>
+        {repos
+          .filter((r) => r.name.includes(phrase))
+          .map((r) => {
+            <li key={r.id}>
+              <a href={r.url}>{r.name}</a>
+            </li>;
+          })}
+      </ul>
+    </section>
+  );
+};
+
+export default GitHub;
